7d4fb7f807479458324fa7723f68b7f3dbe4eed9
RoliverJavier <roliverjavier@gmail.com>
2018-12-21 11:11:01 -0400

    Review Changed
    interface named ValidatorsType with a index signature created
    
    Used a filter to exclude the properties annoted with the metadata design:type and foreach to push in the array.

--- src/services/model-form-builder.service.ts
+++ src/services/model-form-builder.service.ts
@@ -1,6 +1,6 @@
-import { FormGroup, FormControl, Validators, ValidatorFn } from '@angular/forms';
 import 'reflect-metadata';
 import { Injectable } from '@angular/core';
+import { FormGroup, FormControl, Validators, ValidatorFn } from '@angular/forms';
 
 @Injectable()
 export class ModelFormBuilder<T> {
@@ -11,37 +11,40 @@ export class ModelFormBuilder<T> {
 
       const decorators = Reflect.getMetadataKeys(target, propertyKey);
       if (decorators.indexOf('exclude') > 0) continue;
-      
+
       fg.addControl(propertyKey, new FormControl(target[propertyKey]));
       const validators: ValidatorFn[] = [];
 
-      decorators.map((decorator)=>{
-        let validator = this.getValidatorByType(decorator, target, propertyKey);
-        if(validator!== null) validators.push(validator);
-      });
+      decorators
+        .filter(x => x !== 'design:type')
+        .forEach((decorator) => {
+          const validator = this.getValidatorByType(decorator, target, propertyKey);
+          if (validator !== null) validators.push(validator);
+        });
 
       fg.get(propertyKey).setValidators(validators);
     }
+
     return fg;
   }
 
-   getValidatorByType(metadataKey: any, target: Object, propertyKey: string | symbol){
-      let 
-        validator : any,
-        metadataValue: ValidatorFn,
-        validatorsType : any = {
-        'required': ()=> Validators.required,
-        'email': ()=>  Validators.email,
-        'min' : (min : number)=>Validators.min(min),
-        'max' : (max : number)=> Validators.max(max),
-        'pattern' : (pattern : string | RegExp)=> Validators.pattern(pattern),
-        'customValidator' : (custom : ValidatorFn)=> custom,
-       };
-       
-       metadataValue = Reflect.getMetadata(metadataKey, target, propertyKey);
-       validator =  (metadataKey === 'design:type')? null : validatorsType[metadataKey](metadataValue);
-       return validator;
-   }
+  getValidatorByType(metadataKey: string, target: Object, propertyKey: string | symbol): ValidatorFn {
+
+    const validatorsType: ValidatorsType = {
+      'required': _ => Validators.required,
+      'email': _ => Validators.email,
+      'min': (min: number) => Validators.min(min),
+      'max': (max: number) => Validators.max(max),
+      'pattern': (pattern: string | RegExp) => Validators.pattern(pattern),
+      'customValidator': (custom: ValidatorFn) => custom
+    };
+
+    const metadataValue = Reflect.getMetadata(metadataKey, target, propertyKey);
+    return validatorsType[metadataKey](metadataValue);
+  }
 
 }
 
+interface ValidatorsType {
+  [key: string]: ((...args: any) => ValidatorFn);
+}
